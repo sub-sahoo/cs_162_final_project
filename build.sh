64d85a1641b94c42f169660e62825e9b684e3098
Sub Sahoo <cclrsahoo@gmail.com>
2026-03-12 13:41:23 -0700

    Move vercel files into root folder

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ENV="${RUNTIME_ENV:-production}"
-echo "var RUNTIME_ENV = '$ENV';" > visualization/env.js
---
+++ visualization/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+ENV="${RUNTIME_ENV:-production}"
+echo "var RUNTIME_ENV = '$ENV';" > env.js
